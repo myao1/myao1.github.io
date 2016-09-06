239252970087ff9da3d4d29d51c9c76ada852b8c
Michael Yao <yaoninja@gmail.com>
2016-09-06 13:37:36 -0400

    adding 2d transforms buttons

--- scripts/personal.js
+++ scripts/personal.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
 function canvasDrawing() {
 	var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
+	ctx.color = "red";
 	ctx.moveTo(50,50);
 	ctx.lineTo(300, 300);
 	ctx.stroke();
@@ -23,14 +24,43 @@ function moveStuffAround(){
 	var e = document.getElementById();
 }
 
+function toggleDisplay(){
+	$("#moreInfo").toggle('slow');
+}
+
 function changeFontColor(color){
-	var x = document.getElementById("changetextcolor");
-	x.style.color = color;
-	x.style.backgroundColor = "powderblue";
+	// var x = document.getElementById("changetextcolor");
+	// x.style.color = color;
+	// x.style.backgroundColor = "powderblue";
+	var y = $("#changetextcolor");
+	y.css("background-color", "powderblue");
+	y.css("color", color);
+	if(y.hasClass("test")){
+		y.removeClass("test");
+	}else{
+		y.addClass("test");
+	}
+}
+
+function mytranslate(){
+	var x = $("#2dtransform");
+	x.addClass("translate");
+}
+
+function reset2d(){
+	var x = $("2dtransform");
+	if(x.hasClass("translate")){
+		x.removeClass("translate");
+	}
 }
 
 function testChange(){
-	var x = document.getElementById("changetextcolor");
-	x.style.backgroundColor = "powderblue";
-	x.style.borderRadius = "50%";
+	//javascript
+	// var x = document.getElementById("changetextcolor");
+	// x.style.backgroundColor = "powderblue";
+	// x.className += "test";
+
+	//jquery
+	$("#changetextcolor").addClass("test");
+	// x.style.borderRadius = "50%";
 }
