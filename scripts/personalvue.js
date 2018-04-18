ca280609bacc38e6f8610bc60532487db8880bd5
Michael Yao <yaoninja@gmail.com>
2018-04-18 15:02:14 -0400

    cleaning up code in js

--- scripts/personalvue.js
+++ scripts/personalvue.js
@@ -6,17 +6,22 @@ window.onload = function () {
         {id: 3, text: "Change Style", htmlid:"changestyles", link:"#changestyles"},
         {id: 4, text: "Transforms", htmlid:"transforms", link:"#transforms"},
         {id: 5, text: "Animations", htmlid:"animations", link:"#animations"},
+        {id: 8, text: "Geolocation", link: "#geolocation"},
+        {id: 9, text: "Storage", link: "#webstorage"},
         {id: 6, text: "Donuts", htmlid:"donuts", link:"#donuts"},
         {id: 7, text: "Vue", htmlid:"vue", link:"#vue"}
     ];
 
-    Vue.component('vue-nav-li', {
+    var NavigationComponent = Vue.component('vue-nav-li', {
         props: ['navlink'],
         template: '<li><a v-bind:href="navlink.link"><div>{{ navlink.text }}</div></a></li>'
     });
     
     var NavigationVue = new Vue({
         el: "#Vue-nav",
+        components: {
+            'nav-component': NavigationComponent
+        },
         data: {
             navigationItems: navigationTabs
         }
@@ -128,25 +133,9 @@ window.onload = function () {
 
     var donutVue = new Vue({
         el: '#vueDonuts',
-        // computed: {
-        //     emailWithDate: function(){
-        //         if(isWeekday()){
-        //             return this.emailRoot + " tomorrow!";
-        //         }else{
-        //             return this.emailRoot + " Monday!";
-        //         }
-        //     }
-        // },
         data: {
             emailRoot: "mailto:?bcc=Craig.Trulove@perficient.com;Zach.Gay@perficient.com;Venice.Thacker@perficient.com;Will.Sullivan@perficient.com;michael.yao@perficient.com;Chase.Spivey@perficient.com;Matt.Haneburger@perficient.com;Shu.Jackson@perficient.com;Courtney.Dean@perficient.com;Jason.Barrett@perficient.com;george.chang@perficient.com;Nick.Sturdivant@perficient.com;Brian.Ball@perficient.com;Ryan.Selley@perficient.com;TL.Stephanchick@perficient.com;Corey.Smith@perficient.com;Justin.Combs@perficient.com;matt.connolly@perficient.com;bill.veldman@perficient.com;mike.ball@perficient.com;Jeffrey.Correa@perficient.com;Larry.Thomas@perficient.com;ravind.budhiraja@perficient.com;Marshall.Sorenson@perficient.com;Amelie.VonFluegge@perficient.com;Joshua.Hulsey@perficient.com&subject=Donuts!&body=Hey everyone, %0D%0A%0D%0AI'll be bringing in donuts",
         },
-        // mounted: function(){
-        //     if(isWeekday()){
-        //         this.emailDisplay = this.emailRoot + " tomorrow!";
-        //     }else{
-        //         this.emailDisplay = this.emailRoot + " Monday!";
-        //     }
-        // },
         methods: {
             dateTheEmail: function () {
                 if (isWeekday()) {
@@ -157,29 +146,4 @@ window.onload = function () {
             }
         }
     });
-    
-    
-    // new Vue({
-    //     el: '#vuewatchtest',
-    //     data: {
-
-    //     },
-    //     watch: {
-
-    //     }
-    // });
-
-    Vue.component('button-counter',
-        {
-            data: function () {
-                return {
-                    count: 0
-                }
-            },
-            template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-        }
-    );
-
-    new Vue({ el: '#vuecounter' });
-    
 }
